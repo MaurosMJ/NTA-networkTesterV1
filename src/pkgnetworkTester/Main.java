1136f1e61a8c89ad79def36d1bcf548515a5c3f5
Mauros Milach Junior <mauros99milach@gmail.com>
2024-01-02 11:56:57 -0300

    adicionado tratamento de textos

--- src/pkgnetworkTester/Main.java
+++ src/pkgnetworkTester/Main.java
@@ -7,6 +7,8 @@ package pkgnetworkTester;
 
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  *
@@ -28,32 +30,45 @@ public class Main{
     private static String run = "Não informado.";
     
     public static void main(String[] args) throws IOException {
+        
+        telaModulos();
+        
+    }
+    
+    private static void telaModulos () throws IOException{
+        
         String input = "Não informado.";
         
-        while (!input.equals("7")){
+        while (!input.equals("13")){
         
         System.out.println("\nSelecione uma opção de teste de conectividade:"
-                + "\n1 - Atalhos"
+                + "\n1 - JVM Info"
                 + "\n2 - HTTP POST Request"
                 + "\n3 - HTTP GET Request"
                 + "\n4 - Socket Connection"
                 + "\n5 - SMB Protocol Connection"
                 + "\n6 - SMTP Protocol Connection"
                 + "\n7 - Oracle Net Procotol Connection"
-                + "\n8 - Webscraping"
-                + "\n9 - Encerrar aplicação\n\n"
+                + "\n8 - Tabular Data Stream Connection"
+                + "\n9 - MySQL Protocol Connection (TCP\\IP)"
+                + "\n10 - SQL Command Window"
+                + "\n11 - Histórico"
+                + "\n12 - Atalhos"
+                + "\n13 - Encerrar Aplicação"
+                + "\n\n"
                 + "Opção: ");
         
-        input = scanner.nextLine();
+        input = input (true);
         
         switch (input){
             case "1":
+                jvmInfo();
                 break;
             case "2":
-                httpPost ();
+                httpPost();
                 break;
             case "3":
-                httpGET ();
+                httpGET();
                 break;
             case "4":
                 socket();
@@ -68,15 +83,33 @@ public class Main{
                 database();
                 break;
             case "8":
-                webscraping();
                 break;
             case "9":
-                break;//apenas encerra a aplicação.
+                break;
+            case "10":
+                break;
+            case "11":
+                break;
+            case "12":
+                break;
+            case "13":
+                System.exit(0);;//apenas encerra a aplicação.
+            case "exit":
+                System.exit(0);
         }
         
+        }
     }
+    
+    private static String input (boolean trim){
+        String entrada = scanner.nextLine();
         
-    }
+        if(trim){
+        entrada = entrada.replaceAll("\\s+", "");
+        }
+        
+        return entrada;
+    } 
     
     private static void smtpMail (){
         System.out.println("\nHTTP SMTP Protocol:\n");
@@ -89,13 +122,13 @@ public class Main{
                 + "Destinatario: "+destinatario
                 + "Senha: "+senha);    
             
-        System.out.print("Host SMTP: "); host = scanner.nextLine();
-        System.out.print("Porta: "); porta = scanner.nextLine();
-        System.out.print("Protocolo: "); protocol = scanner.nextLine();
-        System.out.print("Remetente: "); remetente = scanner.nextLine();
-        System.out.print("Destinatario: "); destinatario = scanner.nextLine();
-        System.out.print("Senha: "); senha = scanner.nextLine();
-        System.out.print("Confirmar configurações (S|N)? "); run = scanner.nextLine(); 
+        //System.out.print("Host SMTP: "); host = input ();
+        //System.out.print("Porta: "); porta = input ();
+        //System.out.print("Protocolo: "); protocol = input ();
+        //System.out.print("Remetente: "); remetente = input ();
+        //System.out.print("Destinatario: "); destinatario = input ();
+        //System.out.print("Senha: "); senha = input ();
+        //System.out.print("Confirmar configurações (S|N)? "); run = input ();
         }
         smtpConnection smtp = new smtpConnection ();
         smtp.smtpH(host, porta, protocol, remetente, destinatario, senha);
@@ -104,17 +137,19 @@ public class Main{
     private static void httpPost () throws IOException{
         System.out.println("\nHTTP POST REQUEST:\n");
         
-        String url = "Não informado.";
-        String urlP = "Não informado.";
-        String run = "Não informado.";
+        //String url = "Não informado.";
+        //String urlP = "Não informado.";
+        //String run = "Não informado.";
         
         while ((!"S".equals(run))&&(!"s".equals(run))){
         System.out.println("Endereço Destino: "+url
                 + "Parametros Opcionais: "+urlP);
-            
-        System.out.print("Endereço destino: "); url = scanner.nextLine();
-        System.out.print("Parametros opcionais: "); urlP = scanner.nextLine();
-        System.out.print("Confirmar configurações (S|N)? "); run = scanner.nextLine(); 
+        
+        
+        
+        //System.out.print("Endereço destino: "); url = input ();
+        //System.out.print("Parametros opcionais: "); urlP = input ();
+        //System.out.print("Confirmar configurações (S|N)? "); run = input ();
         }
         httpPostConnection http = new httpPostConnection();
         http.hConnect(url, urlP);
@@ -128,8 +163,8 @@ public class Main{
         while ((!"S".equals(run))&&(!"s".equals(run))){
         System.out.println("Endereço destino: "+url);
             
-        System.out.print("Endereço destino: "); url = scanner.nextLine();
-        System.out.print("Confirmar configurações (S|N)? "); run = scanner.nextLine(); 
+        //System.out.print("Endereço destino: "); url = input ();
+        //System.out.print("Confirmar configurações (S|N)? "); run = input ();
         }
         httpGetConnection http = new httpGetConnection();
         http.hConnect(url);
@@ -146,9 +181,9 @@ public class Main{
         System.out.println("Máquina destino: "+url);
         System.out.println("Porta: "+port);
             
-        System.out.print("Informar a máquina destino: "); url = scanner.nextLine();
-        System.out.print("Informar a porta da máquina: "); port = scanner.nextLine();
-        System.out.print("Confirmar configurações (S|N)? "); run = scanner.nextLine(); 
+        //System.out.print("Informar a máquina destino: "); url = input ();
+        //System.out.print("Informar a porta da máquina: "); port = input ();
+        //System.out.print("Confirmar configurações (S|N)? "); run = input ();
         }
         
         socketConnection socket = new socketConnection();
@@ -168,10 +203,10 @@ public class Main{
                 + "Usuário: "
                 + "Senha: ");
             
-        System.out.print("Informar endereço de IP ou nome da máquina: "); host = scanner.nextLine();
-        System.out.print("Informar o Usuário: "); user = scanner.nextLine();
-        System.out.print("Informar o Senha: ");  passW = scanner.nextLine();
-        System.out.print("Confirmar configurações (S|N)? "); run = scanner.nextLine(); 
+        //System.out.print("Informar endereço de IP ou nome da máquina: "); host = input ();
+        //System.out.print("Informar o Usuário: "); user = input ();
+        //System.out.print("Informar o Senha: ");  passW = input ();
+        //System.out.print("Confirmar configurações (S|N)? "); run = input ();
         }
         
         
@@ -187,10 +222,10 @@ public class Main{
         String usuario = "Não informado.";
                 
         while ((!"S".equals(run))&&(!"s".equals(run))){
-        System.out.print("Informar a URL do Banco de dados: "); url = scanner.nextLine();
-        System.out.print("Informar a Usuário do Banco de dados: "); usuario = scanner.nextLine();
-        System.out.print("Informar a Senha do Banco de dados: "); passW = scanner.nextLine();
-        System.out.print("Confirmar configurações (S|N)? "); run = scanner.nextLine();
+        //System.out.print("Informar a URL do Banco de dados: "); url = input ();
+        //System.out.print("Informar a Usuário do Banco de dados: "); usuario = input ();
+        //System.out.print("Informar a Senha do Banco de dados: "); passW = input ();
+        //System.out.print("Confirmar configurações (S|N)? "); run = input ();
         }
         
         oracledbConnection db = new oracledbConnection ();
@@ -198,54 +233,43 @@ public class Main{
             
         }
         
-        private static void webscraping(){
-        System.out.println("\nWEBSCRAPING:\n");
-        String run = "Não informado.";
-        String url = "Não informado.";
         
-        while ((!"S".equals(run))&&(!"s".equals(run))){
-        url = scanner.nextLine();
+        private static String trataCampo (String input){
+        System.out.println("Linha 236: "+ input);
+        int indiceSet = input.toLowerCase().indexOf("set");
+        if (indiceSet != -1) { // Se encontrou a palavra "set"
+            return input.substring(indiceSet + 3); // Obtém o texto após "set" (considerando que "set" tem 3 caracteres)
+        } else {
+            return "Palavra 'set' não encontrada ou sem texto após 'set'.";
         }
-        webscraping web = new webscraping();
-        web.webscrapingM(url);
+                 
         }
         
-        private static void trataCampo (String Command, String input){
-        
-        // Encontrar a posição da palavra "host"
-        int index = input.indexOf(Command);
-        
-        // Verificar se a palavra "host" foi encontrada e pegar o texto após ela
-        if (index != -1 && index + Command.length() < input.length()) {
-            String textoDepoisHost = input.substring(index + Command.length() + 1);
-            System.out.println("Texto após 'host': " + textoDepoisHost);
-        }  
+        private static void atualizaValor (String var){
             
-        }
-        
-        private static void atualizaValor (String key, String valor){
-            
-        switch (key){
+        switch (var){
             case "host":
-            host = valor;    
+            host = input(true);    
             case "port":
-            porta = valor;    
+            porta = input(true);
             case "protocol":
-            protocol = valor;    
+            protocol = input(true); 
             case "remetente":
-            remetente = valor;    
+            remetente = input(true);
             case "destinatario":
-            destinatario = valor;    
+            destinatario = input(true); 
             case "senha":
-            senha = valor;    
+            senha = input(true);
             case "url":
-            url = valor;    
+            url = input(true);
             case "urlP":
-            urlP = valor;    
+            urlP = input(true);
             case "passW":
-            passW = valor;    
+            passW = input(true);   
             case "user":
-            user = valor;    
+            user = input(true);
+            case "exit":
+            System.exit(0);
             
             default:
                 System.out.println("Comando não reconhecido.");
@@ -253,34 +277,21 @@ public class Main{
         
         }
         
-        private static void command (String command, int num) {
+        private static void command (String command) {
             
             if (command.toLowerCase().contains("set")){
-                
+              String var = trataCampo(input(true)); // REMOVE A PALAVRA "SET"
+              atualizaValor(var);
             }
             
-            if (command.toLowerCase().contains("show")){
+            if (command.toLowerCase().contains("run")){
                 
             }
             
         }
         
-        private static void showParam (int option){
-           
-           if (option == 6){
-                System.out.println("\nHost SMTP: "+host
-                + "Porta: "+porta
-                + "Protocolo: "+protocol
-                + "Remetente: "+remetente
-                + "Destinatario: "+destinatario
-                + "Senha: "+senha);    
-
-           }
-           if (option == 1){
-               
-           }
-           if (option == 1){
-               
-           }
+        private static void jvmInfo (){  
+            
+            
         }
 }
